715beea8c9b4baad2a734e9e10fc3bd0e6d56473
Troy Crawford <Troy@lechler.de>
2018-04-06 17:11:41 -0500

    intial file setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+const API_KEY = 'AIzaSyCjUMN-vLAcstl5sMUTt8ZMRIAiLqtdKoU';
+
+const App = () => {
+    return <div>Hello World </div>
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
